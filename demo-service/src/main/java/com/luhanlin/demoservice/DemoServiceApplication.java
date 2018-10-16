a106c3cba7cf10d0e061634132dfc84c295275be
luhanlin <47901673@qq.com>
2018-10-16 13:43:38 +0800

    Mr_lu 提交代码 新建service模块

---
+++ demo-service/src/main/java/com/luhanlin/demoservice/DemoServiceApplication.java
@@ -0,0 +1,12 @@
+package com.luhanlin.demoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoServiceApplication.class, args);
+    }
+}
